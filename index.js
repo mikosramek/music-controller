a8974ac72d2c96f24ac7084045d2cab9e51255d0
miko@pi <miko@miksramek.ca>
2020-03-12 18:35:27 -0400

    So korean will play if it has the file path already??

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const listener = require('./util/internalHost');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(player);
 
-// require('./util/fileReader')('./music');
+//require('./util/fileReader')('./music');
 
 // app.listen(3000);
 app.listen(80, '0.0.0.0', listener);
