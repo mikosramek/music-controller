48d772081d08179030731ad83dabc7b0634bfe17
mikosramek <miko@mikosramek.ca>
2020-03-12 18:37:53 -0400

    oof

--- index.js
+++ index.js
@@ -12,12 +12,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(player);
 
-<<<<<<< HEAD
 // require('./util/fileReader')('./music');
 // require('./util/fileFixer')('./music');
-=======
-//require('./util/fileReader')('./music');
->>>>>>> a8974ac72d2c96f24ac7084045d2cab9e51255d0
 
 // app.listen(3000);
 app.listen(80, '0.0.0.0', listener);
